48bc22621339efb0dbb398c1f42dc8dbe354d144
Adherwp <adheadhe71@gmail.com>
2019-09-30 16:14:54 +0700

    selesai tugas

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -1,8 +1,14 @@
 package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.fragments.Dips;
+import polinema.ac.id.starterchapter05.fragments.Handstand;
+import polinema.ac.id.starterchapter05.fragments.Pushups;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new Handstand(),"HANDSTAND");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    public void handlerPushups(View view) {
+        Pushups check=(Pushups) getSupportFragmentManager().findFragmentByTag("PUSHUP");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        if (check!=null && check.isVisible()){
+
+        }else{
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new Pushups(), "PUSHUP");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void handlerDips(View view) {
+        Dips check=(Dips) getSupportFragmentManager().findFragmentByTag("DIPS");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        if (check!=null && check.isVisible()){
+
+        }else{
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new Dips(), "DIPS");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void handlerHandstand(View view) {
+        Handstand check=(Handstand) getSupportFragmentManager().findFragmentByTag("HANDSTAND");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        if (check!=null && check.isVisible()){
+
+        }else{
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new Handstand(), "HANDSTAND");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
 }
